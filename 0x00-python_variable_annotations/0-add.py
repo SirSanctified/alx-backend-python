7dc2e4273112a68ee4d853cc9b9b8413897cd34c
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-30 10:19:09 +0200

    Define type annotated variables

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-'''
+"""
 Contains a type-annotated function add that takes a float a
 and a float b as arguments and returns their sum as a float.
-'''
+"""
 
 
 def add(a: float, b: float) -> float:
-    '''
+    """
     takes a float a and a float b as arguments
     and returns their sum as a float.
-    '''
+    """
     return a + b
 
 
