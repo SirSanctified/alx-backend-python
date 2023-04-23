c8dc64cc30b05db39d60450cbbcf3c2600cebe1c
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-04-23 16:19:27 +0200

    Test for exceptions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -29,6 +29,22 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ('a',)),
+        ({'a': 1}, ('a', 'b')),
+    ])
+    def test_access_nested_map_exception(
+        self,
+        nested_map: Mapping,
+        path: Sequence
+    ):
+        """
+        Test access_nested_map exception
+        """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+            self.assertEqual(e.exception, KeyError(path[-1]))
+
 
 if __name__ == "__main__":
     unittest.main()
